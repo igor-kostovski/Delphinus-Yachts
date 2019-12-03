b52f1065b6acdb8a66f932e6ea6b249e54bf0345
igor <igor.kostovski@pseudocode.agency>
2019-12-03 23:00:35 +0100

    project setup

---
+++ Delphinus-Yachts.Domain/AppUserManager.cs
@@ -0,0 +1,17 @@
+﻿using Delphinus_Yachts.Domain.Data.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace Delphinus_Yachts.Domain
+{
+    public class AppUserManager : UserManager<User>
+    {
+        public AppUserManager(IUserStore<User> store) : base(store) { }
+    }
+
+    public class AppSignInManager : SignInManager<User, string>
+    {
+        public AppSignInManager(AppUserManager userManager, IAuthenticationManager authenticationManager) : base(userManager, authenticationManager) { }
+    }
+}
