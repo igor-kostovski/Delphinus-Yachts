d33e02840efc27200718530dbffa15971053ca30
igor <igor.kostovski@pseudocode.agency>
2019-12-10 22:08:57 +0100

    added mvc controller

---
+++ Delphinus-Yachts/Controllers/MvcController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Delphinus_Yachts.Controllers
+{
+    public class MvcController : Controller
+    {
+        public ActionResult Index(string entity)
+        {
+            return View(entity + "/Index");
+        }
+
+        public ActionResult Edit(string entity)
+        {
+            return View(entity + "/Edit");
+        }
+    }
+}
