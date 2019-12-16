a453b518e39a0c016e3c4d3ceab90f13e4af91d5
igor <igor.kostovski@pseudocode.agency>
2019-12-16 20:49:35 +0100

    refactored bundle config, added authorization for mvc controller

--- Delphinus-Yachts/App_Start/BundleConfig.cs
+++ Delphinus-Yachts/App_Start/BundleConfig.cs
@@ -8,23 +8,16 @@ namespace Delphinus_Yachts
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Client/Dependencies/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Client/Dependencies/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Client/Dependencies/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Client/Dependencies/Scripts/bootstrap.js",
-                      "~/Client/Dependencies/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/dependencies").Include(
+                "~/Client/Dependencies/Scripts/jquery-{version}.js",
+                "~/Client/Dependencies/Scripts/jquery.validate*",
+                "~/Client/Dependencies/Scripts/modernizr-*",
+                "~/Client/Dependencies/Scripts/bootstrap.js",
+                "~/Client/Dependencies/Scripts/respond.js"
+                ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css"));
+                "~/Content/bootstrap.css"));
         }
     }
 }
