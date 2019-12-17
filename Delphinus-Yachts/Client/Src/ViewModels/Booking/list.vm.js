35cf35e034bc2af843b00b54c61d041e4eb2a4c9
igor <igor.kostovski@pseudocode.agency>
2019-12-17 20:50:47 +0100

    applied bootstrap vue tables

--- Delphinus-Yachts/Client/Src/ViewModels/Booking/list.vm.js
+++ Delphinus-Yachts/Client/Src/ViewModels/Booking/list.vm.js
@@ -2,27 +2,32 @@
     var vm = new Vue({
         el: "#table",
         data: {
-            items: [],
-            columns: [
-                { field: 'ProductID' },
-                { field: 'ProductName', title: 'Product Name' },
-                { field: 'UnitPrice', title: 'Unit Price' }
-            ]
+            rows: [],
+            columns: [{
+                label: "Booking number",
+                name: "number"
+            },
+            {
+                label: "Start date",
+                name: "startDate"
+            },
+            {
+                label: "Start location",
+                name: "startLocation"
+            },
+            {
+                label: "End location",
+                name: "endLocation"
+            },
+            {
+                label: "Status",
+                name: "status"
+            }]
         },
-        methods: {
-            createRandomData(count) {
-                const productNames = ['Chai', 'Chang', 'Syrup', 'Apple', 'Orange', 'Banana', 'Lemon', 'Pineapple', 'Tea', 'Milk'];
-                const unitPrices = [12.5, 10.1, 5.3, 7, 22.53, 16.22, 20, 50, 100, 120]
-
-                return Array(count).fill({}).map((_, idx) => ({
-                    ProductID: idx + 1,
-                    ProductName: productNames[Math.floor(Math.random() * productNames.length)],
-                    UnitPrice: unitPrices[Math.floor(Math.random() * unitPrices.length)]
-                }));
-            }
-        },
-        mounted() {
-            this.items = this.createRandomData(50);
+        created: function () {
+            axios.get(baseUrl + "api/bookings").then((res) => {
+                this.rows = res.data;
+            })
         }
     });
 })();
