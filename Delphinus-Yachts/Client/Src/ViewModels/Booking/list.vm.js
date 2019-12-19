7f85c724a3526f971a68251617fa5d5c77f42a8b
igor <igor.kostovski@pseudocode.agency>
2019-12-19 14:06:35 +0100

    added loading spinner

--- Delphinus-Yachts/Client/Src/ViewModels/Booking/list.vm.js
+++ Delphinus-Yachts/Client/Src/ViewModels/Booking/list.vm.js
@@ -2,6 +2,7 @@
     var vm = new Vue({
         el: "#table",
         data: {
+            isLoading: false,
             rows: [],
             columns: [{
                 label: "Booking number",
@@ -22,11 +23,18 @@
             {
                 label: "Status",
                 name: "status"
-            }]
+            }],
+            config: {
+                show_refresh_button: false,
+                per_page_options: [5, 10],
+                pagination_info: false
+            }
         },
         created: function () {
+            this.isLoading = true;
             axios.get(baseUrl + "api/bookings").then((res) => {
                 this.rows = res.data;
+                this.isLoading = false;
             })
         }
     });
