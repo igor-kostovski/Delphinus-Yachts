d07a96fdc14a54e4d5e40ef60fa132b22c3a13f6
igor <igor.kostovski@pseudocode.agency>
2019-12-19 14:14:39 +0100

    added table filter and data models

---
+++ Delphinus-Yachts.Domain/Models/Table/DataAndCount.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Delphinus_Yachts.Domain.Models.Table
+{
+    public class DataAndCount<T>
+    {
+        public IEnumerable<T> Data { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Delphinus-Yachts.Domain/Models/Table/TableFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace Delphinus_Yachts.Domain.Models
+{
+    public class TableFilter
+    {
+        public int Limit { get; set; }
+        public int Page { get; set; }
+        public string Query { get; set; }
+    }
+}
