33d5d6c75afc596d3d2a88f2bd218971ba710c9f
igor <igor.kostovski@pseudocode.agency>
2020-01-12 19:30:39 +0100

    added migration for adding price column to contracts table

--- Delphinus-Yachts.Domain/Data/Entities/Contract.cs
+++ Delphinus-Yachts.Domain/Data/Entities/Contract.cs
@@ -6,6 +6,7 @@ namespace Delphinus_Yachts.Domain.Data.Entities
     public class Contract
     {
         public int Id { get; set; }
+        public double Price { get; set; }
         public double Discount { get; set; }
         public double APA { get; set; }
         public double Tax { get; set; }
---
+++ Delphinus-Yachts.Domain/Migrations/202001121829082_AddedPriceColumnToContracts.cs
@@ -0,0 +1,18 @@
+﻿namespace Delphinus_Yachts.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPriceColumnToContracts : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Contracts", "Price", c => c.Double(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Contracts", "Price");
+        }
+    }
+}
