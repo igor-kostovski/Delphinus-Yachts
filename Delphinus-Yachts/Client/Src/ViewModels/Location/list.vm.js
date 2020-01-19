1240b868198fd0f5ff26b6a6d33523d126166fc6
igor <igor.kostovski@pseudocode.agency>
2020-01-19 12:45:52 +0100

    added locations and routes layout and frontend logic

--- Delphinus-Yachts/Client/Src/ViewModels/Location/list.vm.js
+++ Delphinus-Yachts/Client/Src/ViewModels/Location/list.vm.js
@@ -1,1 +1,31 @@
-﻿
+﻿(function () {
+    var vm = new Vue({
+        el: "#table",
+        data: {
+            isLoading: false,
+            rows: [],
+            count: 0,
+            columns: constants.locationListColumns,
+            config: constants.tableConfig
+        },
+        methods: {
+            getTableData(searchText = null) {
+                this.isLoading = true;
+                axios.get(baseUrl + "api/locations", {
+                    params: {
+                        "query": searchText,
+                        "page": constants.tablePaginationConfig.page,
+                        "limit": constants.tablePaginationConfig.perPage
+                    }
+                }).then((res) => {
+                    this.rows = res.data.data;
+                    this.count = res.data.count;
+                    this.isLoading = false;
+                });
+            }
+        },
+        created: function () {
+            this.getTableData();
+        }
+    });
+})();
