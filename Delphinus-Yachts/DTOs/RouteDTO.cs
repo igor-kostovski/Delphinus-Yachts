a6e621da865afee9414fda4e6d78d85b09f91acb
igor <igor.kostovski@pseudocode.agency>
2020-01-19 11:53:43 +0100

    added route and location dtos and automapper profiles

---
+++ Delphinus-Yachts/DTOs/RouteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delphinus_Yachts.DTOs
+{
+    public class RouteDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<LocationDTO> Locations { get; set; }
+    }
+}
