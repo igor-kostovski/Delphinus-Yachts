3e0a2d292e40cd97edbde5b3890e1023f4f0d500
igor <igor.kostovski@pseudocode.agency>
2020-01-19 12:16:41 +0100

    added locations controller

---
+++ Delphinus-Yachts/Api/LocationsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using AutoMapper;
+using Delphinus_Yachts.Domain.Services;
+
+namespace Delphinus_Yachts.Api
+{
+    public class LocationsController : ApiController
+    {
+        private readonly LocationService _locationService;
+        private readonly IMapper _mapper;
+
+        public LocationsController(LocationService locationService, IMapper mapper)
+        {
+            _locationService = locationService;
+            _mapper = mapper;
+        }
+
+
+    }
+}
