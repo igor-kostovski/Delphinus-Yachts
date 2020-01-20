921cea7e259b1e41715c2fe22ebd943480f90f2a
igor <igor.kostovski@pseudocode.agency>
2020-01-20 22:38:00 +0100

    added statistics controller dto and automapper profile

---
+++ Delphinus-Yachts/Api/StatisticsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using AutoMapper;
+using Delphinus_Yachts.Domain.Services;
+using Delphinus_Yachts.DTOs;
+
+namespace Delphinus_Yachts.Api
+{
+    public class StatisticsController : ApiController
+    {
+        private readonly StatisticsService _statisticsService;
+        private readonly IMapper _mapper;
+
+        public StatisticsController(StatisticsService statisticsService, IMapper mapper)
+        {
+            _statisticsService = statisticsService;
+            _mapper = mapper;
+        }
+
+        public IHttpActionResult Get()
+        {
+            var model = _statisticsService.Get();
+
+            return Ok(_mapper.Map<StatisticsDTO>(model));
+        }
+    }
+}
---
+++ Delphinus-Yachts/DTOs/StatisticsDTO.cs
@@ -0,0 +1,12 @@
+﻿using Delphinus_Yachts.Domain.Models.Statistics;
+
+namespace Delphinus_Yachts.DTOs
+{
+    public class StatisticsDTO
+    {
+        public double MonthlyEarnings { get; set; }
+        public double AnnualEarnings { get; set; }
+        public int TotalNumberOfPassengers { get; set; }
+        public CountByStatus CountByStatus { get; set; }
+    }
+}
