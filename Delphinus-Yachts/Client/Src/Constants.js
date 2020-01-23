2b7de1305036901be775d1d4398d4471b2e426e0
igor <igor.kostovski@pseudocode.agency>
2020-01-23 02:30:17 +0100

    added pagination to bookings table

--- Delphinus-Yachts/Client/Src/Constants.js
+++ Delphinus-Yachts/Client/Src/Constants.js
@@ -57,21 +57,16 @@
     }];
 
     constants.tableConfig = {
-        pagination: false,
+        pagination: true,
         card_mode: false,
         show_refresh_button: false,
-        per_page_options: [5, 10],
+        per_page_options: [10],
         pagination_info: false,
         show_reset_button: false,
         server_mode: true,
         global_search: {
             visibility: false
         }
-    };
-
-    constants.tablePaginationConfig = {
-        perPage: 10,
-        page: 1
     };
 
     constants.datePickerOptions = {
